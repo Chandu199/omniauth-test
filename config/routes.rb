0e97b8df82251850303ffc4f2f13c9e3c156e275
Chandrakanth N <programmer@Chandrakanths-MacBook-Pro.local>
2017-07-28 12:16:19 +0530

    added functionality of doing omniauth with github and FB

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  root 'home#index'
 
   get 'home/profile'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'auth/:provider/callback', to: "sessions#create"
+  delete 'sign_out', to: "sessions#destroy", as: 'sign_out'
+
+
 end
